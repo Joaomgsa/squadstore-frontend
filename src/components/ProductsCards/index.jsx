662a92ce1b80ea0624698f78769a8c9e2d5773a3
Joaomgsa <joaomgsa@gmail.com>
2023-01-08 00:06:35 -0300

    compontent Cards with list

--- src/components/ProductsCards/index.jsx
+++ src/components/ProductsCards/index.jsx
@@ -1,11 +1,30 @@
-
+import {Link} from 'react-router-dom';
 
 
 export default function ProductsCards() {
-    const productsLabels = ['Presentes de Natal', 'Renove sua Casa', 'Aproveite as Promoções','Eletronicos', 'Computadores', 'Livros']
+    
+    function ProductCardsList(props){
+        const labels = props.labels;
+        const listLabels = labels.map((label)=>
+        <li className="" key={label}>
+            <Link to={"/"}>{label}</Link>
+        </li>
+        );
+        return(
+            <>
+            <ul>
+                {listLabels}
+            </ul>
+            </>
+        );
+    }
+    const labels = ['Presentes de Natal', 'Renove sua Casa', 'Aproveite as Promoções','Eletronicos', 'Computadores', 'Livros'];
+    
     return(
         <>
-        
+        <div>
+        <ProductCardsList labels={labels} />
+        </div>
         </>
     )
 }
