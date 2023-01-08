30fbde90d2ea2bba5ddc366b8e331f6187cf9067
Joaomgsa <joaomgsa@gmail.com>
2023-01-07 23:37:46 -0300

    compontent Cards

---
+++ src/components/ProductsCards/index.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function ProductsCards() {
+    const productsLabels = ['Presentes de Natal', 'Renove sua Casa', 'Aproveite as Promoções','Eletronicos', 'Computadores', 'Livros']
+    return(
+        <>
+        
+        </>
+    )
+}
